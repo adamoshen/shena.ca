3521f6426bd07902cd874210b4038edee1a965f1
Adam Shen <adamshen1@gmail.com>
2023-01-20 02:18:57 -0500

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,1 +1,5 @@
 blogdown::new_site(theme = "wowchemy/starter-hugo-academic")
+
+# For future reference
+# https://github.com/wowchemy/starter-hugo-academic/tree/69d48921b74f014c71c56b0f76d4f9fc37762ec9
+# https://github.com/wowchemy/starter-hugo-academic/archive/69d48921b74f014c71c56b0f76d4f9fc37762ec9.zip
